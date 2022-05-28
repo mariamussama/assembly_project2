b714f15f370bdc4ccbc7fe2b5856229c3a6a9d5f
Salma1004-code <SalmaEmad@aucegypt.edu>
2022-05-28 21:36:47 +0200

    Bonus 2
    Bonus 2

--- Source.cpp
+++ Source.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 struct block
 {
+	string check = ""; // "0" means instruction memory address  "1" means data memory address
 	string tag = "";
 	string offset = "";
 	string line_idx = "";
@@ -42,15 +43,17 @@ vector<block> read_mem(ifstream& file_name, vector<block>& Memory, int offset, i
 		cout << "Can't open file!!" << endl;
 	else
 	{
-		string address;
+		string address, text;
 		cout << "File opend successfully!!" << endl;
 		cout << "-----------------------------------------------------------------------------------------------------------------------" << endl;
-		while (getline(file_name, address))
+		while (getline(file_name, text))
 		{
+			B.check = text.substr(0, 1);
+			address = text.substr(2, 32);
 			B.data = address;
 			B.tag = address.substr(0, tag);
 			B.line_idx = address.substr(tag, index);
-			B.offset = address.substr(tag + index , offset);
+			B.offset = address.substr(tag + index, offset);
 			string temp = B.line_idx;
 			bitset<32> b(temp);
 			B.index = b.to_ulong();
@@ -69,14 +72,10 @@ vector<block> read_mem(ifstream& file_name, vector<block>& Memory, int offset, i
 
 
 
-void direct_mapping(vector<block>Memory, vector<line>& Cache, double CC)
+void direct_mapping(block temp, vector<block>Memory, vector<line>& Cache, double CC)
 {
 	double hit = 0, miss = 0;
-	for (int i = 0; i < 20; i++)
-	{
-		int idx = rand() % Memory.size();
-		block temp = Memory[idx];
-		cout << "Processor: " << temp.tag << " " << temp.index << " " << temp.data << endl;
+	
 		if (Cache[temp.index].valid_bit == "0")
 		{
 			miss++;
@@ -115,26 +114,16 @@ void direct_mapping(vector<block>Memory, vector<line>& Cache, double CC)
 			cout << c.valid_bit << "\t" << c.tag << "\t" << c.data << endl;
 		}
 		cout << "-----------------------------------------------------------------------------------------------------------------------" << endl;
-	}
+	
 
 }
 
-void Set_Full(vector<block>Memory, vector<Set>& Setcache, double CC, int numset)
+void Set_Full(block temp ,vector<block>Memory, vector<Set>& Setcache, double CC, int numset)
 {
-	//vector<line> Cache;
+	
 	double hit = 0, miss = 0;
-	for (int i = 0; i < 20; i++)
-	{
-		int idx = rand() % Memory.size();
-		block temp = Memory[idx];
-		cout << "Processor: " << temp.tag << " " << temp.address << " " << temp.data << endl;
+	
 
-		/*string addr = temp.data;
-		bitset<32> b(addr);
-		int address = b.to_ulong();
-
-		int set = address % numset;*/
-		//Set temp2 = Setcache[temp.address];
 		bool check = false;
 		for (int m = 0; m < Setcache[temp.address].S.size(); m++)
 		{
@@ -144,7 +133,7 @@ void Set_Full(vector<block>Memory, vector<Set>& Setcache, double CC, int numset)
 				{
 					hit++; check = true;
 				}
-			}	
+			}
 		}
 		if (!check)
 		{
@@ -160,7 +149,7 @@ void Set_Full(vector<block>Memory, vector<Set>& Setcache, double CC, int numset)
 			else
 			{
 				int k = rand() % Setcache[temp.address].S.size();
-				Setcache[temp.address].S[k]=l;
+				Setcache[temp.address].S[k] = l;
 			}
 		}
 
@@ -178,9 +167,9 @@ void Set_Full(vector<block>Memory, vector<Set>& Setcache, double CC, int numset)
 		cout << "(VB)\t(Tag)\t\t\t\t(Data)" << endl;
 		for (int j = 0; j < Setcache.size(); j++)
 		{
-			if (Setcache[j].size!=1)
-				cout << "Set" << j<<endl;
-			for (int k = 0; k < Setcache[j].S.size() ; k++)
+			if (Setcache[j].size != 1)
+				cout << "Set" << j << endl;
+			for (int k = 0; k < Setcache[j].S.size(); k++)
 			{
 				line c = Setcache[j].S[k];
 				cout << c.valid_bit << "\t" << c.tag << "\t" << c.data << endl;
@@ -188,7 +177,7 @@ void Set_Full(vector<block>Memory, vector<Set>& Setcache, double CC, int numset)
 
 		}
 		cout << "-----------------------------------------------------------------------------------------------------------------------" << endl;
-	}
+	
 }
 
 
@@ -196,9 +185,11 @@ int main()
 {
 	ifstream file_name("memory.txt");
 	vector<block>Memory;
-	vector<line> Cache;
-	vector <Set> Setcache;
-	int S, L, M=1;
+	vector<line> Cache_inst;
+	vector <Set> Setcache_inst;
+	vector<line> Cache_data;
+	vector <Set> Setcache_data;
+	int S, L, M = 1;
 	double CC;
 	int choice, offset, index, tag;
 	cout << "The American University in Cairo" << endl << "Assembly Language - Memory Hierarchy Simulator" << endl << "Mariam Abdelaziz 900196082" << endl << "Salma Abdelhalim 900193718" << endl << "Abdelhalim Ali 900193539" << endl;
@@ -226,7 +217,8 @@ int main()
 	cout << "-----------------------------------------------------------------------------------------------------------------------" << endl;
 	if (choice == 1)
 	{
-		Cache.resize(C);
+		Cache_inst.resize(C);
+		Cache_data.resize(C);
 		offset = log2(L);//disp
 		index = log2(C);//index
 		tag = 32 - index - offset;//tag
@@ -235,7 +227,7 @@ int main()
 	if (choice == 2)
 	{
 		offset = log2(L);//disp
-		index = log2(C/M);//index
+		index = log2(C / M);//index
 		tag = 32 - index - offset;//tag
 	}
 
@@ -247,15 +239,55 @@ int main()
 		tag = 32 - offset;//tag
 	}
 
-	Setcache.resize(M);
-	for (int i = 0; i < Setcache.size(); i++)
-		Setcache[i].size = C / M;
+	Setcache_inst.resize(M);
+	for (int i = 0; i < Setcache_inst.size(); i++)
+		Setcache_inst[i].size = C / M;
+
+	Setcache_data.resize(M);
+	for (int i = 0; i < Setcache_data.size(); i++)
+		Setcache_data[i].size = C / M;
 
 	//cout << tag;
 	Memory = read_mem(file_name, Memory, offset, index, tag, M, choice);
 	if (choice == 1)
-		direct_mapping(Memory, Cache, CC);
+	{
+		for (int i = 0; i < 20; i++)
+		{
+			int idx = rand() % Memory.size();
+			block temp = Memory[idx];
+			cout << "Processor: " << temp.tag << " " << temp.index << " " << temp.data << endl;
+			if (temp.check == "0")
+			{
+				cout << "Instruction Cache accessed" << endl;
+				direct_mapping(temp, Memory, Cache_inst, CC);
+			}
+			else
+			{
+				cout << "Data Cache accessed" << endl;
+				direct_mapping(temp, Memory, Cache_data, CC);
+				
+			}
+		}
+	}
 	else
-		Set_Full(Memory, Setcache, CC, M);
+	{
+		for (int i = 0; i < 20; i++)
+		{
+			int idx = rand() % Memory.size();
+			block temp = Memory[idx];
+			cout << "Processor: " << temp.tag << "   " << temp.address << "   " << temp.data << endl;
+			if (temp.check == "0")
+			{
+				cout << "Instruction Cache accessed" << endl;
+				Set_Full(temp, Memory, Setcache_inst, CC, M);
+				
+			}
+			else
+			{
+				cout << "Data Cache accessed" << endl;
+				Set_Full(temp, Memory, Setcache_data, CC, M);
+			}
+		}
+	}
 	return 0;
 }
